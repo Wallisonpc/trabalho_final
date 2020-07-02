27d4ac14e4e5581f03aa7142cb69e4aff2127b3a
unknown <wallison.pc@gmail.com>
2020-07-02 09:37:17 -0300

    commit

---
+++ mrcondo/src/app/register/register.page.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { AuthenticateService } from '../services/authenticate.service';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage implements OnInit {
+
+
+  validations_form: FormGroup;
+  errorMessage: string = '';
+  successMessage: string = '';
+
+  validation_messages = {
+    'email': [
+      { type: 'required', message: 'Email requerido.' },
+      { type: 'pattern', message: 'Digite um Email Valido.' }
+    ],
+    'password': [
+      { type: 'required', message: 'Senha requerido.' },
+      { type: 'minlength', message: 'Senha com mínimo 5 caracteres.' }
+    ]
+  };
+
+  constructor(
+    private navCtrl: NavController,
+    private authService: AuthenticateService,
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit() {
+    this.validations_form = this.formBuilder.group({
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.minLength(5),
+        Validators.required
+      ])),
+    });
+  }
+
+  tryRegister(value) {
+    this.authService.registerUser(value)
+      .then(res => {
+        console.log(res);
+        this.errorMessage = "";
+        this.successMessage = "Sua conta foi criada. Por favor faça login.";
+      }, err => {
+        console.log(err);
+        this.errorMessage = err.message;
+        this.successMessage = "";
+      })
+  }
+
+  goLoginPage() {
+    this.navCtrl.navigateBack('');
+  }
+
+
+}
---
+++ mrcondo/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAwe4wV-YBX4m0hoEkWRYyS34uH_jN45Bo",
+    authDomain: "mrcondominio-f33cb.firebaseapp.com",
+    databaseURL: "https://mrcondominio-f33cb.firebaseio.com",
+    projectId: "mrcondominio-f33cb",
+    storageBucket: "mrcondominio-f33cb.appspot.com",
+    messagingSenderId: "196071306327",
+    appId: "1:196071306327:web:e306e72b848d0f10951f12"
+
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
